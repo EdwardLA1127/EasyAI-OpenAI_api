5bf8a6d8bd79744af80c952104c8964aa7a9a007
LA <407805173@qq.com>
2023-04-05 13:25:30 +0800

    test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+'''
+Author: Edward_LA
+Email: wowofla@sina.com
+Date: 2023-04-05 13:22:23
+Description: 
+Note: 
+LastEditor: Edward_LA
+LastEditTime: 2023-04-05 13:22:36
+'''
+import os
+
+print('hi')
